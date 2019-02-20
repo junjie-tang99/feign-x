93125f2dc05c8a4f1919c8370c970daca9e19ff9
tangjunjie <tangjunjie@migu.cn>
2019-02-20 15:26:55 +0800

    add @Inherited on annotatons

--- src/main/java/feign/annotation/EnableRpcServer.java
+++ src/main/java/feign/annotation/EnableRpcServer.java
@@ -2,6 +2,7 @@ package feign.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Import;
 
 import feign.RpcServerAutoConfiguration;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
--- src/main/java/feign/annotation/FeignClientExt.java
+++ src/main/java/feign/annotation/FeignClientExt.java
@@ -2,6 +2,7 @@ package feign.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -12,6 +13,7 @@ import org.springframework.core.annotation.AliasFor;
 
 import feign.enumerate.ProtocolType;
 
+@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
